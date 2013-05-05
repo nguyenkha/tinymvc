53338382f43dcd2646d11f0bcd12eaef08642f98
Kha N. Do <donguyenkha@gmail.com>
2013-05-05 09:37:34 +0700

    Add auto render disable sample

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -2,8 +2,11 @@
 require_once 'Controller.php';
 
 class IndexController extends Controller {
-   function indexAction() {
+  function indexAction() {
+    // Disable auto render
+    // $this->_autoRender = false;
     $this->_view->layout->title = 'Tiny MVC framework for PHP';
     $this->_view->message = 'Hello world';
+    // $this->_view->render('index/index');
   }
 }
