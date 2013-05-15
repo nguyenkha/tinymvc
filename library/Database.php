9dc5fde88f2c2243a8c59413c7dfc5d8bb82cfc0
Đỗ Nguyên Kha <donguyenkha@gmail.com>
2013-05-15 07:36:59 +0700

    Update Database.php

--- library/Database.php
+++ library/Database.php
@@ -12,7 +12,7 @@ class Database {
     if (self::$_instance === null) {
       # Connect to database
       $config = self::$_config;
-      self::$_instance = new PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['dbname'] . ';charset=utf8', $config['user'], $config['password']);
+      self::$_instance = new PDO('mysql:host=' . $config['host'] . ';port=' . $config['port'] . ';dbname=' . $config['dbname'] . ';charset=utf8', $config['user'], $config['password']);
     }
     return self::$_instance;
   }
